ebd21a1ed2eee325516386dd21e4bf86359cf60b
kanotan <kanon.ayuayu@gmail.com>
2019-05-06 09:04:54 +0900

    time機能を追加

--- handler.rb
+++ handler.rb
@@ -3,3 +3,7 @@ require 'json'
 def hello(event:, context:)
   { statusCode: 200, body: JSON.generate('Go Serverless v1.0! Your function executed successfully!') }
 end
+
+def time(event:, context:)
+  { statusCode: 200, body: JSON.generate("#{Time.now}") }
+end
